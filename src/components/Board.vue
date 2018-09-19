c521da7fd583ec2a11ed0eda0c013b10ef00f483
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2018-09-20 08:12:38 +0900

    created stone component

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,17 +3,21 @@
 		<table border="1">
 			<tbody>
 				<tr v-for="i in 8">
-					<td v-for="j in 8" class="cell" :id="coordinateVal(i,j)"></td>
+					<td v-for="j in 8" class="cell" :id="coordinateVal(i,j)">
+						<stone v-if="isExist"></stone>
+					</td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
 </template>
 <script type="text/javascript">
+	import Stone from "./Stone.vue"
 	export default {
 		data() {
 			return {
-				alphabetArr: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+				alphabetArr: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+				isExist: true
 			}
 		},
 		methods: {
@@ -22,10 +26,14 @@
 				let a = this.alphabetArr[index];
 				return a + i;
 			}
-		}
+		},
+		components: {
+    		Stone
+  		}
 	}
 </script>
 <style type="text/css">
 	.box {padding: 10px; margin-bottom: 1em}
-	table {width: 360px; height: 360px; background: green}
+	.cell {width: 30px; height: 20px; padding: 0;}
+	table {width: 360px; height: 360px; background: green; table-layout: fixed;}
 </style>
---
+++ src/components/Stone.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="stone">●</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+<style type="text/css">
+ /*.stone {font-size: 35px; width: 30px; height: 30px; text-align: center;}*/
+ .stone {font-size: 30px; text-align: center; margin: 0;}
+</style>

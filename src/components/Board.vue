b36e8ec62125cb20e4b1a4192fd5364a03e9ce9f
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2018-09-20 07:54:32 +0900

    created board component

---
+++ src/components/Board.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="box">
+		<table border="1">
+			<tbody>
+				<tr v-for="i in 8">
+					<td v-for="j in 8" class="cell" :id="coordinateVal(i,j)"></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data() {
+			return {
+				alphabetArr: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+			}
+		},
+		methods: {
+			coordinateVal (i, j) {
+				let index = j-1;
+				let a = this.alphabetArr[index];
+				return a + i;
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	.box {padding: 10px; margin-bottom: 1em}
+	table {width: 360px; height: 360px; background: green}
+</style>
